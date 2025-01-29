1eab589001a06c80b609e1cd5fc24d37275f1d8f
zdravka.p.goranova <zdravka.p.goranova@gmail.com>
2025-01-29 16:40:41 +0200

    create ThemeProvider

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,9 @@
+import '../../App.css';
 import PropTypes from 'prop-types';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+
+import { useTheme } from '@mui/material/styles';
 import { createTheme } from '@mui/material/styles';
 
 import Home from '../../Components/Home/Home.jsx';
@@ -15,6 +18,7 @@ import LocalPhoneRoundedIcon from '@mui/icons-material/LocalPhoneRounded';
 import { AppProvider } from '@toolpad/core/AppProvider';
 import { DashboardLayout } from '@toolpad/core/DashboardLayout';
 import { useDemoRouter } from '@toolpad/core/internal';
+
 
 const NAVIGATION = [
   {
@@ -58,7 +62,7 @@ const demoTheme = createTheme({
     values: {
       xs: 0,
       sm: 600,
-      md: 600,
+      md: 900,
       lg: 1200,
       xl: 1536,
     },
@@ -69,7 +73,7 @@ function DemoPageContent({ pathname }) {
   return (
     <Box
       sx={{
-        py: 4,
+        py: 5,
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
@@ -88,20 +92,28 @@ DemoPageContent.propTypes = {
 
 function Navbar(props) {
   const { window } = props;
-
-  const router = useDemoRouter('/dashboard');
-
-  // Remove this const when copying and pasting into your project.
+  const router = useDemoRouter('/');
   const demoWindow = window !== undefined ? window() : undefined;
 
+  const theme = useTheme();
+
   return (
-    // preview-start
     <AppProvider
       navigation={NAVIGATION}
       branding={{
-        logo: <img src="https://mui.com/static/logo.png" alt="MUI logo" />,
-        icon: <PersonRoundedIcon />,
-        title: 'PortfolioZG',
+        logo: (
+          <Typography
+            sx={{
+              color: theme.palette.secondary.main,
+              fontWeight: 'bold',
+              fontSize: '1.5rem',
+            }}
+          >
+            PortfolioZG
+          </Typography>
+        ),
+
+        title: '',
         homeUrl: '/',
       }}
       router={router}
@@ -112,17 +124,11 @@ function Navbar(props) {
         <DemoPageContent pathname={router.pathname} />
       </DashboardLayout>
     </AppProvider>
-    // preview-end
   );
 }
 
 Navbar.propTypes = {
-  /**
-   * Injected by the documentation to work in an iframe.
-   * Remove this when copying and pasting into your project.
-   */
   window: PropTypes.func,
 };
 
 export default Navbar;
-
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import './index.css';
+
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme.js';
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
-)
+);
