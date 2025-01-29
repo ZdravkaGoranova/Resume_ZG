c4f3bea3fd1e2d3a18a5eb20e3315f5717dee4d7
zdravka.p.goranova <zdravka.p.goranova@gmail.com>
2025-01-30 01:33:26 +0200

    add new routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import { Routes, Route } from 'react-router-dom';
 import Footer from './Components/Footer/Footer.jsx';
 import Header from './Components/Header/Header.jsx';
 import Home from './Components/Home/Home.jsx';
+import About from './Components/About/About.jsx';
+import Projects from './Components/Projects/Projects.jsx';
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<Home />} />
-        <Route path="/projects" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
         <Route path="*" element={<Home />} />
       </Routes>
 
