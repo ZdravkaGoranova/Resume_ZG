511c255d6f2d63e10bb374b72db4b580adcaa6e5
zdravka.p.goranova <zdravka.p.goranova@gmail.com>
2025-02-05 19:51:00 +0200

    add new file

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 import dotenv from 'dotenv';
 import express from 'express';
 import { MongoClient, ServerApiVersion } from 'mongodb';
@@ -14,7 +13,6 @@ const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/Cluster0';
 app.use(cors());
 app.use(express.json());
 
-
 const client = new MongoClient(MONGO_URI, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -29,10 +27,14 @@ async function run() {
     console.log(
       'Pinged your deployment. You successfully connected to MongoDB!',
     );
-    
-const data = await client.db().collection('ZdravkaGoranova').find().toArray();
 
-console.log('!!!', data);
+    const data = await client
+      .db()
+      .collection('ZdravkaGoranova')
+      .find()
+      .toArray();
+
+    console.log('!!!', data);
   } catch (error) {
     console.error('Не може да се свърже с MongoDB:', error);
   }
@@ -46,9 +48,10 @@ app.get('/', async (req, res) => {
       .collection('ZdravkaGoranova')
       .find()
       .toArray();
-    console.log('Данни, които се изпращат:', data); 
+    console.log('Данни, които се изпращат:', data);
 
     res.json({ message: 'Сървърът работи 🚀', data });
+  } catch (error) {
     console.error('Грешка при извличането на данни:', error);
     res.status(500).json({
       message: 'Грешка при извличането на данни от MongoDB.',
@@ -56,7 +59,6 @@ app.get('/', async (req, res) => {
     });
   }
 });
-
 
 app.listen(PORT, () => {
   console.log(`🚀 Сървърът стартира на http://localhost:${PORT}`);
