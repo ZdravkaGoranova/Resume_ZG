aff26c94e36527d67daa9560b18d98e6d60d0fbc
zdravka.p.goranova <zdravka.p.goranova@gmail.com>
2025-02-06 12:22:50 +0200

    update add buttons for social networks

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,27 +2,16 @@ import PropTypes from 'prop-types';
 
 import Avatar from '@mui/material/Avatar';
 import Stack from '@mui/material/Stack';
-import Paper from '@mui/material/Paper';
+import Button from '@mui/material/Button';
+// import { Button as BaseButton, buttonClasses } from '@mui/base/Button';
 
-import { styled } from '@mui/material/styles';
-import { Button as BaseButton, buttonClasses } from '@mui/base/Button';
-
+import IconButton from '@mui/material/IconButton';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import SystemUpdateAltIcon from '@mui/icons-material/SystemUpdateAlt';
 
 export default function Home({ data }) {
   const userData = data?.data?.[0] || {};
-
-  const Item = styled(Paper)(({ theme }) => ({
-    // backgroundColor: '#fff',
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    // color: theme.palette.text.secondary,
-    ...theme.applyStyles('dark', {
-      backgroundColor: '#1A2027',
-    }),
-  }));
 
   return (
     <>
@@ -30,19 +19,16 @@ export default function Home({ data }) {
         <Avatar
           alt="zdravka Goranova Image"
           src="/image-zdravka-goranova.png"
-          sx={{ width: 156, height: 156 }}
+          sx={{
+            width: 220,
+            height: 220,
+            borderRadius: '50%',
+            boxShadow: '0 4px 10px rgba(0, 0, 0, 0.2)',
+            border: '3px solid #ffc134',
+          }}
         />
       </Stack>
-      <div>
-        <Stack
-          direction={{ xs: 'column', sm: 'row' }}
-          spacing={{ xs: 1, sm: 2, md: 4 }}
-        >
-          <Item>Item 1</Item>
-          <Item>Item 2</Item>
-          <Item>Item 3</Item>
-        </Stack>
-      </div>
+
       <div>
         {/* <pre>{data ? JSON.stringify(data, null, 2) : 'Loading...'}</pre> */}
       </div>
@@ -56,10 +42,29 @@ export default function Home({ data }) {
         <p>{userData?.principalInfo || 'Loading...'}</p>
       </div>
 
-      <Stack spacing={2} direction="row" textAlign="center">
+      <Stack
+        direction={{ xs: 'column', sm: 'row' }}
+        spacing={{ xs: 2, sm: 2, md: 2 }}
+        textAlign="center"
+        padding="50px"
+        alignItems="center"
+        justifyContent="center"
+        sx={{ width: '100%' }}
+      >
         <Button
+          variant="contained"
+          endIcon={<SystemUpdateAltIcon />}
           href="/Zdravka_Goranova_CV_Resume.pdf"
           download="Zdravka_Goranova_CV_Resume.pdf"
+          style={{
+            borderRadius: '20px',
+            color: 'white',
+            transition: 'all 150ms ease',
+            cursor: 'pointer',
+            border: `1px solid ${yellow[700]}`,
+            backgroundColor: yellow[500],
+            padding: '9px 16px',
+          }}
         >
           Download CV
         </Button>
@@ -67,12 +72,34 @@ export default function Home({ data }) {
         <IconButton
           href={userData?.contacts?.linkedin || 'Loading...'}
           rel="noopener noreferrer"
+          style={{
+            width: '48px',
+            height: '48px',
+            borderRadius: '50%',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            transition: 'all 150ms ease',
+            cursor: 'pointer',
+            border: `2px solid ${yellow[500]}`,
+          }}
         >
           <LinkedInIcon />
         </IconButton>
         <IconButton
           href={userData?.contacts?.github || 'Loading...'}
           target="_blank"
+          style={{
+            width: '48px',
+            height: '48px',
+            borderRadius: '50%',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            transition: 'all 150ms ease',
+            cursor: 'pointer',
+            border: `2px solid ${yellow[500]}`,
+          }}
         >
           <GitHubIcon />
         </IconButton>
@@ -84,93 +111,60 @@ Home.propTypes = {
   data: PropTypes.any,
 };
 
-const blue = {
-  200: '#99CCFF',
-  300: '#66B2FF',
-  400: '#3399FF',
-  500: '#007FFF',
-  600: '#0072E5',
-  700: '#0066CC',
+const yellow = {
+  200: '#ffe34d',
+  300: '#ffdf33',
+  400: '#ffdb1a',
+  500: '#ffc134',
+  600: '#ffd700',
+  700: '#d8b600',
 };
 
-const grey = {
-  50: '#F3F6F9',
-  100: '#E5EAF2',
-  200: '#DAE2ED',
-  300: '#C7D0DD',
-  400: '#B0B8C4',
-  500: '#9DA8B7',
-  600: '#6B7A90',
-  700: '#434D5B',
-  800: '#303740',
-  900: '#1C2025',
-};
+// const grey = {
+//   50: '#F3F6F9',
+//   100: '#E5EAF2',
+//   200: '#DAE2ED',
+//   300: '#C7D0DD',
+//   400: '#B0B8C4',
+//   500: '#9DA8B7',
+//   600: '#6B7A90',
+//   700: '#434D5B',
+//   800: '#303740',
+//   900: '#1C2025',
+// };
 
-const Button = styled(BaseButton)(
-  ({ theme }) => `
-  font-family: 'IBM Plex Sans', sans-serif;
-  font-weight: 600;
-  font-size: 0.875rem;
-  line-height: 1.5;
-  background-color: ${blue[500]};
-  padding: 8px 16px;
-  border-radius: 8px;
-  color: white;
-  transition: all 150ms ease;
-  cursor: pointer;
-  border: 1px solid ${blue[500]};
-  box-shadow: 0 2px 1px ${
-    theme.palette.mode === 'dark'
-      ? 'rgba(0, 0, 0, 0.5)'
-      : 'rgba(45, 45, 60, 0.2)'
-  }, inset 0 1.5px 1px ${blue[400]}, inset 0 -2px 1px ${blue[600]};
+// const Button = styled(BaseButton)(
+//   ({ theme }) => `
+//     endIcon={<SystemUpdateAltIcon />}
+//   font-weight: 600;
+//   font-size: 0.875rem;
+//   line-height: 1.5;
+//   background-color: ${yellow[500]};
+//   padding: 9px 16px;
+//   border-radius:20px;
+//   color: white;
+//   transition: all 150ms ease;
+//   cursor: pointer;
+//   border: 1px solid ${yellow[700]};
+//   box-shadow: 0 2px 1px ${
+//     theme.palette.mode === 'dark'
+//       ? 'rgba(0, 0, 0, 0.5)'
+//       : 'rgba(45, 45, 60, 0.2)'
+//   }, inset 0 1.5px 1px ${yellow[400]}, inset 0 -2px 1px ${yellow[600]};
 
-  &:hover {
-    background-color: ${blue[600]};
-  }
+//   &:hover {
+//     background-color: ${yellow[600]};
+//   }
 
-  &.${buttonClasses.active} {
-    background-color: ${blue[700]};
-    box-shadow: none;
-    transform: scale(0.99);
-  }
+//   &.${buttonClasses.active} {
+//     background-color: ${yellow[700]};
+//     box-shadow: none;
+//     transform: scale(0.99);
+//   }
 
-  &.${buttonClasses.focusVisible} {
-    box-shadow: 0 0 0 4px ${theme.palette.mode === 'dark' ? blue[300] : blue[200]};
-    outline: none;
-  }
-
-  &.${buttonClasses.disabled} {
-    background-color: ${theme.palette.mode === 'dark' ? grey[700] : grey[200]};
-    color: ${theme.palette.mode === 'dark' ? grey[200] : grey[700]};
-    border: 0;
-    cursor: default;
-    box-shadow: none;
-    transform: scale(1);
-  }
-  `,
-);
-
-const IconButton = styled(BaseButton)(
-  ({ theme }) => `
-  width: 48px; /* Прави бутона кръгъл */
-  height: 48px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border-radius: 50%; /* Заоблени ръбове */
-  background-color: ${blue[500]};
-  color: white;
-  border: none;
-  cursor: pointer;
-  transition: background-color 0.3s ease;
-
-  &:hover {
-    background-color: ${blue[600]};
-  }
-
-  &:active {
-    background-color: ${blue[700]};
-  }
-`,
-);
+//   &.${buttonClasses.focusVisible} {
+//     box-shadow: 0 0 0 4px ${theme.palette.mode === 'dark' ? yellow[300] : yellow[200]};
+//     outline: none;
+//   }
+//   `,
+// );
