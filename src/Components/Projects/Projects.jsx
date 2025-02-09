dcdf403a3a35fdf6e186087f09a785718ceccdc4
zdravka.p.goranova <zdravka.p.goranova@gmail.com>
2025-02-09 15:51:41 +0200

    update ProjectCard styles and add into Projects component and det data from database

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,17 +1,62 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 import ProjectCard from '../ProjectCard/ProjectCard.jsx';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid2';
+import { experimentalStyled as styled } from '@mui/material/styles';
 
-export default function Projects() {
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(2),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+  ...theme.applyStyles('dark', {
+    backgroundColor: '#1A2027',
+  }),
+}));
+
+export default function Projects({ data }) {
+  const userData = data?.data?.[0] || {};
+
+  const projects = Array.isArray(userData?.projects) ? userData.projects : [];
+
+  console.log('Projects array:', projects);
+
   return (
     <>
       <h1>Projects</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem tempora
-        consectetur cupiditate temporibus aliquid sint nostrum voluptatem, ut
-        unde quos magnam, dolorum harum voluptate eum sequi quam aut? Deleniti,
-        maiores.
-      </p>
-      <ProjectCard/>
+
+      <Grid
+        container
+        spacing={{ xs: 2, md: 3 }}
+        columns={{ xs: 4, sm: 8, md: 12 }}
+      >
+        {projects.length > 0 ? (
+          projects.map(
+            (project, index) =>
+              project.name && (
+                <Grid item xs={12} sm={4} md={4} key={index}>
+                  <Paper elevation={3}>
+                    <ProjectCard
+                      name={project.name}
+                      title={project.title}
+                      linkGithub={project.linkGithub}
+                      linkLive={project.linkLive}
+                      image={project.image}
+                    />
+                  </Paper>
+                </Grid>
+              ),
+          )
+        ) : (
+          <p>No projects found.</p>
+        )}
+      </Grid>
     </>
   );
 }
+Projects.propTypes = {
+  data: PropTypes.any,
+};
