f6d0398f9fba68bb878c9ddaf1d6f8f6fd16f66b
zdravka.p.goranova <zdravka.p.goranova@gmail.com>
2025-02-19 14:37:38 +0200

    update architecture in app

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
-import { ThemeProviderComponent } from './ThemeContext';
+import { ThemeProviderComponent } from './ThemeContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
