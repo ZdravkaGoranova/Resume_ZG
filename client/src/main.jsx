c9484bed1580b1228131e2eadca792a8bed4dc63
zdravka.p.goranova <zdravka.p.goranova@gmail.com>
2025-02-22 16:57:10 +0200

    add BrowserRouter

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,17 +3,15 @@ import './index.css';
 
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-// import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { ThemeProviderComponent } from './ThemeContext.jsx';
-import { HashRouter } from 'react-router-dom';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <BrowserRouter basename="/"> */}
-    <HashRouter>
+    <BrowserRouter basename="/">
       <ThemeProviderComponent>
         <App />
       </ThemeProviderComponent>
-    </HashRouter>
-    {/* </BrowserRouter> */}
+    </BrowserRouter>
   </StrictMode>,
 );
