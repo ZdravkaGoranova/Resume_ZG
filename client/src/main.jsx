e67c35f4fd78a3d65dc9e5c0efa60e3bd76b5f37
zdravka.p.goranova <zdravka.p.goranova@gmail.com>
2025-02-22 15:09:09 +0200

    add HashRouter

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,15 +4,16 @@ import './index.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import { ThemeProviderComponent } from './ThemeContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/">
+    <HashRouter>
       <ThemeProviderComponent>
         <App />
       </ThemeProviderComponent>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 );
